2bbcfd83687d2d7bc598858c5365ba2131026350
Fabian Sguiglia <fsguiglia@gmail.com>
2019-03-04 19:48:32 -0300

    Initial commit

---
+++ cbcsBuffer.js
@@ -0,0 +1,55 @@
+var pb = new PolyBuffer(jsarguments[1]);
+var dFileIndex = new Dict(jsarguments[1]);
+var sampleList = new Array();
+var sampleRate = new Array();
+var posList = new Array();
+var srcCount = 1;
+
+function openwindow() {
+    pb.open();
+}
+
+
+function closewindow() {
+    pb.wclose();
+}
+
+function append(fileName) {
+    pb.append(fileName);
+    dFileIndex.set(fileName, srcCount);
+    sampleRate.push(parseFloat(pb.dump()[pb.dump().length - 1]) / 1000);
+    srcCount = srcCount + 1;
+}
+
+function fileIndex(fileName) {
+    outlet(0, dFileIndex.get(fileName));
+}
+
+function clear() {
+    pb.clear();
+    dFileIndex.clear();
+    sampleList = [];
+    posList = [];
+    sampleRate = [];
+}
+
+
+function newGrain() {
+    var curSamples = arrayfromargs("newGrain", arguments);
+    sampleList = [];
+    posList = [];
+
+    for(i = 0; i < parseInt(curSamples[1]); i++) {
+        sampleList.push(curSamples[i * 3 + 2]);
+        posList.push(curSamples[i * 3 + 3]);
+    }
+}
+
+
+function getGrain(){
+    if(sampleList.length > 0) {
+        var i = Math.floor(Math.random() * sampleList.length);
+        var srcIndex = parseInt(dFileIndex.get(sampleList[i]));
+        outlet(0, [jsarguments[1] + "." + srcIndex, posList[i] / sampleRate[srcIndex - 1]]);
+    }
+}
